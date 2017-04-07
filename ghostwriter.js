a028fd8d2df0b9eebbe76656b0fcd12149024818
Unknown <kgiron@mail.csuchico.edu>
2017-04-06 21:40:09 -0700

    fixed spacing

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,18 @@
 function writeTweet()
 {
-  
+  var randIndex;
+  var sentence;
+  var intro = ["Have you ever noticed", "Sometimes I wonder", "What's the deal with", "Why is it that"];
+  var body = [" cafeteria food is bland.", " this weather is perfect!", " gas is too expensive." , " schools almost out." , "  the sky is blue."];
+  var end = ["Oh well." , "Too bad." , "Hmm."];
+  randIndex = randomUpTo(intro.length - 1);
+  sentence = intro[randIndex];
+  randIndex = randomUpTo(body.length - 1);
+  sentence = sentence + " " + body[randIndex];
+  randIndex = randomUpTo(end.length - 1);
+  sentence = sentence + " " + end[randIndex];
+  document.getElementById("tweet").innerHTML = sentence;
+
 }
 
 //Generates a random whole number between 0 and max (inclusive)
